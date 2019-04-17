766ae64f5aa831395dc332546ae649480f434a25
yaoxinc <yaoxinc@yonyou.com>
2019-04-17 11:37:40 +0800

    filter为空允许用户传入filterData

--- js/refs/RefCoreWithInput.js
+++ js/refs/RefCoreWithInput.js
@@ -6,7 +6,7 @@ import FormControl from 'bee-form-control';
 import 'bee-form-control/build/FormControl.css'
 import 'bee-input-group/build/InputGroup.css'
 import 'bee-icon/build/Icon.css'
-// import {InputGroup,FormControl} from 'tinper-bee';
+import { is } from 'immutable';
 import '../../css/refcorewithInput.css';
 import '../utils/polyfill_shim.js'
 const refValParse = (value) => {
@@ -22,6 +22,21 @@ const refValParse = (value) => {
     }catch(e) {
         return {refname: '', refpk: ''};
     }
+}
+
+const propTypes = {
+	matchUrl: PropTypes.string,
+	param: PropTypes.object,
+	style: PropTypes.object,
+	// displayField: PropTypes.string,//显示内容的键
+	valueField: PropTypes.string,//真实 value 的键
+	filterUrl: PropTypes.string,
+	value: PropTypes.string,
+	wrapClassName: PropTypes.string,
+	canClickGoOn: PropTypes.func,
+	canInputGoOn: PropTypes.func,
+	filterData:PropTypes.array,
+	filterUrlFunc:PropTypes.func,
 }
 
 const defaultProps = {
@@ -45,20 +60,11 @@ const defaultProps = {
 	filterUrl: '',
 	wrapClassName: '',
 	canClickGoOn:()=>{return true;},
-	canInputGoOn:()=>{return true;}
+	canInputGoOn:()=>{return true;},
+	filterData:[],
+	filterUrlFunc:(value)=>{},
 }
-const propTypes = {
-	matchUrl: PropTypes.string,
-	param: PropTypes.object,
-	style: PropTypes.object,
-	// displayField: PropTypes.string,//显示内容的键
-	valueField: PropTypes.string,//真实 value 的键
-	filterUrl: PropTypes.string,
-	value: PropTypes.string,
-	wrapClassName: PropTypes.string,
-	canClickGoOn: PropTypes.func,
-	canInputGoOn: PropTypes.func,
-}
+
 
 const FilterItem = (props) => {
 	return (
@@ -78,6 +84,23 @@ const FilterItem = (props) => {
 	);
 }
 
+const getFilterData = (data,valueField,displayField) =>{
+	let filterItems = [], filterDataMap = {};
+	data.forEach(item => {
+		let values = item[valueField];
+		//displayField 存在两种形态，通过字符匹配和函数匹配来获得展示的字段
+		let names = '';
+		if (typeof displayField === 'function') {
+			names = displayField(item);
+		} else {
+			names = displayField.format(item);
+		}
+		filterItems.push(<FilterItem key={values} text={names} value={values} />);
+		filterDataMap[values] = item;
+	});
+	return {filterItems,filterDataMap}
+}
+
 class RefCoreWithInput extends Component {
 	constructor(props) {
 		super(props);
@@ -88,6 +111,7 @@ class RefCoreWithInput extends Component {
 			savedShow: valueMap.refname,
 			filterText: '',
 			filterItems: [],
+			filterData:props.filterUrl?[]:(props.filterData || []),
 			filterDataMap: {},
 			filtering: false,
 			showModal: false
@@ -103,20 +127,44 @@ class RefCoreWithInput extends Component {
 	// 	return true;
 	// }
 	componentWillReceiveProps(nextProps) {
+		let {valueField, displayField = "{refname}",filterUrl} = this.props;
 		if (nextProps.value !== this.props.value) {
 				let valueMap = refValParse(nextProps.value) || {}; 
-				let { valueField } = this.props;
+				// let { valueField } = this.props;
 				let { checkedArray } = this.state;
 				let diffValue = checkedArray.some(item=>{
 					return !Boolean(~valueMap.refpk.indexOf(item[valueField]));
 				});
-				this.setState({
-					checkedArray: diffValue ? [] : checkedArray,
-					savedShow: valueMap.refname
-				}, ()=>{
-					this.handleChange(nextProps.value);
-				})
+				if(!is(this.state.filterData,nextProps.filterData)&&!filterUrl){
+					let {filterItems,filterDataMap} = getFilterData(nextProps.filterData,valueField,displayField)
+					this.setState({
+						filterItems,
+						filterDataMap,
+						filterData:nextProps.filterData,
+						checkedArray: diffValue ? [] : checkedArray,
+						savedShow: valueMap.refname
+					}, ()=>{
+						this.handleChange(nextProps.value);
+					})
+				}else{
+					this.setState({
+						checkedArray: diffValue ? [] : checkedArray,
+						savedShow: valueMap.refname
+					}, ()=>{
+						this.handleChange(nextProps.value);
+					})
+				}
+				
 		}else{
+			//filterUrl不存在，只传入filterData
+			if(!is(this.state.filterData,nextProps.filterData)&&!filterUrl){
+				let {filterItems,filterDataMap} = getFilterData(nextProps.filterData,valueField,displayField)
+				this.setState({
+					filterItems,
+					filterDataMap,
+					filterData:nextProps.filterData,
+				})
+			}
 			return false;
 		}
 		return true;
@@ -213,29 +261,20 @@ class RefCoreWithInput extends Component {
 		})
 	}
 	onFilter = (content) => {
-
-		if (!content) return;
 		let { filterUrl, param, valueField, displayField = "{refname}" } = this.props;
-
+		//20190417这里修改逻辑，如果filterUrl存在，使用下面逻辑，否则回调然后用户传数据进来
+		if(!filterUrl){
+			this.props.filterUrlFunc(content);
+			return false;
+		}
+		if (!content) return;
 		get(filterUrl, {
 			...param,
 			refCode: param.refCode,
 			content: content
 		}).then((response) => {
 			let { data } = response;
-			let filterItems = [], filterDataMap = {};
-			data.forEach(item => {
-				let values = item[valueField];
-				//displayField 存在两种形态，通过字符匹配和函数匹配来获得展示的字段
-				let names = '';
-				if (typeof displayField === 'function') {
-					names = displayField(item);
-				} else {
-					names = displayField.format(item);
-				}
-				filterItems.push(<FilterItem key={values} text={names} value={values} />);
-				filterDataMap[values] = item;
-			});
+			let {filterItems,filterDataMap} = getFilterData(data,valueField,displayField);
 			this.setState({
 				filterItems,
 				filterDataMap
