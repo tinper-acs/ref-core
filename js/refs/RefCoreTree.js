e44d542d15fe98542d4e733a6a2fb34fd115260c
yaoxinc <yaoxinc@yonyou.com>
2019-05-27 11:29:42 +0800

    加上了本地搜索

--- js/refs/RefCoreTree.js
+++ js/refs/RefCoreTree.js
@@ -20,23 +20,23 @@ export default class RefCoreTree extends Component {
 			checkedKey: []
 		};
 		this.children = [];
-		let { nodeKeys = nodeKeysFunc, displayField = "refname",lazyModal, onLoadData} = props;
+		let { nodeKeys = nodeKeysFunc, displayField = "refname",lazyModal, onLoadData,searchValue} = props;
 		let isDisplayFieldFunction = false,isLazyModal=false;
 		if (typeof displayField === 'function') {
 			isDisplayFieldFunction = true;
 		}
 		if(lazyModal || typeof(onLoadData)==='function') isLazyModal= true;
-		this.children = this.loop(props.data || [],nodeKeys,displayField,isDisplayFieldFunction,isLazyModal);
+		this.children = this.loop(props.data || [],nodeKeys,displayField,isDisplayFieldFunction,isLazyModal,searchValue);
 	}
 	componentWillReceiveProps(nextProps, nextState){
     if(!is(nextState, this.state) || !is(nextProps, this.props)){
-			let { nodeKeys = nodeKeysFunc, displayField = "refname",lazyModal, onLoadData} = nextProps;
+			let { nodeKeys = nodeKeysFunc, displayField = "refname",lazyModal, onLoadData,searchValue} = nextProps;
 			let isDisplayFieldFunction = false,isLazyModal=false;
 			if (typeof displayField === 'function') {
 				isDisplayFieldFunction = true;
 			}
 			if(lazyModal || typeof(onLoadData)==='function') isLazyModal= true;
-      this.children = this.loop(nextProps.data || [],nodeKeys,displayField,isDisplayFieldFunction,isLazyModal);
+      this.children = this.loop(nextProps.data || [],nodeKeys,displayField,isDisplayFieldFunction,isLazyModal,searchValue);
       return true;
     }else{
       return false;
@@ -76,77 +76,49 @@ export default class RefCoreTree extends Component {
 		this.props.checkAllchildrenFun(v, id)
 	}
 
-	loop = (datas,nodeKeys,displayField,isDisplayFieldFunction,isLazyModal) => {
+	loop = (datas,nodeKeys,displayField,isDisplayFieldFunction,isLazyModal,searchValue='') => {
 			// let { nodeKeys = nodeKeysFunc, displayField = "refname" } = this.props;
 			return datas.map((item, i) => {
         let key = nodeKeys(item, i);
 				let text = isDisplayFieldFunction?displayField(item): displayField.format(item);
+				let title = <div className="ref-core-tree-node-text">{text}</div> ;
+				if(!!searchValue){
+					const index =text.search(searchValue);
+					const beforeStr =text.substr(0, index);
+					const afterStr =text.substr(index + searchValue.length);
+					if(index>-1){
+						title = (
+							<span>
+								{beforeStr}
+								<span className={' ref-core-tree-node-text uTreeSearchableFilter'}>{searchValue}</span>
+									{afterStr}
+								</span>
+							) 
+					}
+				}
 				let isLeafAttr ={};
 				if(isLazyModal) isLeafAttr.isLeaf = !!item.isLeaf
 				if (item.children && item.children.length) {
 					return <TreeNode 
 					className="ref-core-tree-node" 
 					key={key} 
-					title={<div className="ref-core-tree-node-text">{text}</div>} 
+					title={title} 
 					// title={<div className="ref-core-tree-node-text">{text}{ checkable ? '' : <i className="ref-core-tree-node-selected" />}</div>} 
 					attr={item}  
 					{...isLeafAttr}
 					>
-					{this.loop(item.children,nodeKeys,displayField,isDisplayFieldFunction,isLazyModal)}
+					{this.loop(item.children,nodeKeys,displayField,isDisplayFieldFunction,isLazyModal,searchValue)}
 					</TreeNode>;
 				}
 				return <TreeNode 
 					className="ref-core-tree-node"
 					key={key} 
-					title={<div className="ref-core-tree-node-text">{text}</div>} 
+					title={title} 
 					attr={item} 
 					{...isLeafAttr}
 				/>;
 			});
-		}
-
-
-		// loop = datas => {
-		// 	let { nodeKeys = nodeKeysFunc, displayField = "refname" } = this.props;
-		// 	return datas.map((item, i) => {
-    //     let key = nodeKeys(item, i);
-    //     let text = '';
-    //     if (typeof displayField === 'function') {
-    //       text = displayField(item);
-    //     } else {
-    //       text = displayField.format(item);
-    //     }
-		// 		if (item.children && item.children.length) {
-		// 			return <TreeNode 
-		// 			className="ref-core-tree-node" 
-		// 			key={key} 
-		// 			title={<div className="ref-core-tree-node-text">{text}</div>} 
-		// 			// title={<div className="ref-core-tree-node-text">{text}{ checkable ? '' : <i className="ref-core-tree-node-selected" />}</div>} 
-		// 			attr={item}  
-		// 			isLeaf={!!item.isLeaf} 
-		// 			>
-		// 			{this.loop(item.children)}
-		// 			</TreeNode>;
-		// 		}
-		// 		return <TreeNode 
-		// 			className="ref-core-tree-node"
-		// 			key={key} 
-		// 			title={<div className="ref-core-tree-node-text">{text}</div>} 
-		// 			attr={item} 
-		// 			isLeaf={!!item.isLeaf} 
-		// 		/>;
-		// 	});
-		// }
-	// looploadData = datas => {
-
-	// 	let { parentNodeDisableCheck } = this.props;
-	// 	return datas.map((item) => {
-	// 		if (item.children && item.children.length) {
-	// 			return <TreeNode className="ref-core-tree-node" key={item.id} title={item.refname} attr={item} disableCheckbox={parentNodeDisableCheck}>{this.looploadData(item.children)}</TreeNode>;
-	// 		}
-	// 		return <TreeNode className="ref-core-tree-node"  key={item.id} title={item.refname} attr={item} isLeaf={item.isLeaf} />;
-	// 	});
-	// }
+	}
 	render() {
 		let { data, show = true, ...others } = this.props;
 
